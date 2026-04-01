b961958215945b67f7cafb3f65f05ef8dcae1444
João Paulo <joaopwii@gmail.com>
2026-04-01 13:02:16 -0300

    Criando o primeiro componente

---
+++ hello-vue/src/components/Meucomponente.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Olá mundo Vue.js</h1>
+        <br>
+        <p>Meu primeiro componente Vue.js</p>
+    </div>
+</template>
+
+<style>
+</style>
+
+<script setup>
+</script>
