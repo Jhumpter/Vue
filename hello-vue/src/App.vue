91761b99fe231e2da113257a17ba86e64ca05fb3
João Paulo <joaopwii@gmail.com>
2026-04-01 13:14:58 -0300

    Trabalhando com v-model

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,7 +1,22 @@
 <script setup>
 import meuComponente from './components/Meucomponente.vue'
+import Vmodel from './components/Vmodel.vue';
 </script>
 
 <template>
-  <meuComponente/>
+  <div class="main">
+    <meuComponente/>
+    <Vmodel/>
+  </div>
+  
 </template>
+
+<style>
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
---
+++ hello-vue/src/components/Vmodel.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <input v-model="mensagem"/>
+        <p>Mensagem: <strong>{{ mensagem }}</strong></p>
+    </div>
+</template>
+
+<style>
+</style>
+
+<script setup>
+import { ref } from 'vue'
+//ref cria uma variável reativa, acessado por mensagem.value no script, mas no template é só mensagem
+//o v-model liga o input à variável
+const mensagem = ref('E aí?')
+</script>
